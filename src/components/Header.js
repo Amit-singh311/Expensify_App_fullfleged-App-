7819b393e35402f79e64142c0e164010fc1590f0
Amit Singh <ammysingh786@gmail.com>
2018-09-12 19:26:40 +0530

    production erbpack enalable and ssh key added to the github

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 const Header = () => (
 		<div>
-			<h2>Expensify</h2>
+			<h2>Expensify</h2>			
 			<NavLink to="/" activeClassName="is-active" exact={true}> Home Page </NavLink>
 			<NavLink to="/create" activeClassName="is-active">Add Expense Page</NavLink>
 		    <NavLink to="/help" activeClassName="is-active">HelpPage</NavLink>
